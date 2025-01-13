9f7ffa109869fe0b2750eb7fb047f58bcc8dd8b9
Nikolay Churlyaev <nick@churly.com>
2025-01-13 17:31:31 -0500

    Add project files.

---
+++ Mindbox.Data.Linq.Tests/InheritanceConfigurationTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Mindbox.Data.Linq.Mapping;
+
+namespace Mindbox.Data.Linq.Tests
+{
+	[TestClass]
+	public class InheritanceConfigurationTests
+	{
+		[TestMethod]
+		public void AttributeInheritanceWorks()
+		{
+			var mappingSource = new MindboxMappingSource(
+				new MindboxMappingConfiguration(),
+				new Dictionary<string, bool>(),
+				Array.Empty<string>);
+			var metaTable = mappingSource.GetModel(typeof(DataContext)).GetTable(typeof(RootEntityWithInheritanceMapping));
+
+			Assert.IsTrue(metaTable.RowType.IsInheritanceDefault);
+			Assert.AreEqual(2, metaTable.RowType.InheritanceTypes.Count);
+		}
+
+		[TestMethod]
+		public void ConfigurationInheritanceWorks()
+		{
+			var configuration = new MindboxMappingConfiguration();
+			configuration.AddInheritance<RootEntityWithInheritanceMapping, TestEntity3>("3");
+			var mappingSource = new MindboxMappingSource(
+				configuration,
+				new Dictionary<string, bool>(),
+				Array.Empty<string>);
+			var metaTable = mappingSource.GetModel(typeof(DataContext)).GetTable(typeof(RootEntityWithInheritanceMapping));
+
+			Assert.IsTrue(metaTable.RowType.IsInheritanceDefault);
+			Assert.AreEqual(3, metaTable.RowType.InheritanceTypes.Count);
+		}
+	}
+}
---
+++ Mindbox.Data.Linq.Tests/TrackedObjectLofFormatterTests.cs
@@ -0,0 +1,110 @@
+﻿using System.Data.Linq;
+using System.Data.Linq.Mapping;
+using Mindbox.Data.Linq.Mapping;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using static System.Data.Linq.ChangeTracker;
+using static System.Data.Linq.ChangeTracker.StandardChangeTracker;
+using System.Data.Linq.Logging;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Reflection;
+using Moq;
+using static System.Data.Linq.ChangeProcessor;
+
+namespace Mindbox.Data.Linq.Tests
+{
+	[TestClass]
+	public class TrackedObjectLofFormatterTests
+	{
+		[TestMethod]
+		public void TrackedObject_ЛоггируетсяСНеобходимымиСвойствами()
+		{
+			var exception = new InvalidOperationException("CycleDetected");
+			var expected = "\nTrackedObject.Current.TypeName = RootEntityWithInheritanceMapping\nTrackedObject.Current:\nCurrent.Id = 0\nTrackedObject.IsInteresting = False\nTrackedObject.IsDeleted = False\nTrackedObject.IsModified = False\nTrackedObject.IsDead = False\nTrackedObject.IsWeaklyTracked = False\n";
+
+			var trackedObjectMock = new Mock<TrackedObject>(MockBehavior.Strict);
+			trackedObjectMock.Setup(tom => tom.Current).Returns(new RootEntityWithInheritanceMapping());
+			trackedObjectMock.Setup(tom => tom.IsInteresting).Returns(false);
+			trackedObjectMock.Setup(tom => tom.IsDeleted).Returns(false);
+			trackedObjectMock.Setup(tom => tom.IsModified).Returns(false);
+			trackedObjectMock.Setup(tom => tom.IsDead).Returns(false);
+			trackedObjectMock.Setup(tom => tom.IsWeaklyTracked).Returns(false);
+
+			var trackedObjectLogFormatter = new TrackedObjectLogFormatter();
+			var trackedObject = trackedObjectMock.Object;
+
+			trackedObjectLogFormatter.LogTrackedObject(trackedObject, "TrackedObject", exception);
+
+			var data = exception.Data["TrackedObject"].ToString();
+			Assert.AreEqual(expected, data);
+		}
+
+		[TestMethod]
+		public void ListTrackedObject_Логгируется()
+		{
+			var cycleException = new InvalidOperationException("CycleDetected");
+			var expected = "\nTrackedObject.Current.TypeName = RootEntityWithInheritanceMapping\nTrackedObject.Current:\nCurrent.Id = 0\nTrackedObject.IsInteresting = False\nTrackedObject.IsDeleted = False\nTrackedObject.IsModified = False\nTrackedObject.IsDead = False\nTrackedObject.IsWeaklyTracked = False\n";
+
+			var trackedList = new List<TrackedObject>();
+			for (int i = 0; i < 10; i++)
+			{
+				var trackedObjectMock = new Mock<TrackedObject>(MockBehavior.Strict);
+				trackedObjectMock.Setup(tom => tom.Current).Returns(new RootEntityWithInheritanceMapping());
+				trackedObjectMock.Setup(tom => tom.IsInteresting).Returns(false);
+				trackedObjectMock.Setup(tom => tom.IsDeleted).Returns(false);
+				trackedObjectMock.Setup(tom => tom.IsModified).Returns(false);
+				trackedObjectMock.Setup(tom => tom.IsDead).Returns(false);
+				trackedObjectMock.Setup(tom => tom.IsWeaklyTracked).Returns(false);
+
+				trackedList.Add(trackedObjectMock.Object);
+			}
+
+			var trackedObjectLogFormatter = new TrackedObjectLogFormatter();
+			trackedObjectLogFormatter.LogTrackedList(trackedList, "TrackedList", cycleException);
+
+
+			for (int i = 0; i < 10; i++)
+			{
+				var data = cycleException.Data[$"{i} element in TrackedList"].ToString();
+
+				Assert.AreEqual(expected, data);
+			}
+		}
+
+		[TestMethod]
+		public void DictionaryTrackedObjectState_Логгируется()
+		{
+			var cycleException = new InvalidOperationException("CycleDetected");
+			var expected = "\nHas state: After\nTrackedObject.Current.TypeName = RootEntityWithInheritanceMapping\nTrackedObject.Current:\nCurrent.Id = 0\nTrackedObject.IsInteresting = False\nTrackedObject.IsDeleted = False\nTrackedObject.IsModified = False\nTrackedObject.IsDead = False\nTrackedObject.IsWeaklyTracked = False\n";
+
+			var trackedList = new Dictionary<TrackedObject, VisitState>();
+			for (int i = 0; i < 10; i++)
+			{
+				var metaTypeMock = new Mock<MetaType>();
+				metaTypeMock.Setup(mtm => mtm.Name).Returns("MetaTypeName");
+
+				var trackedObjectMock = new Mock<TrackedObject>();
+				trackedObjectMock.Setup(tom => tom.Type).Returns(metaTypeMock.Object);
+				trackedObjectMock.Setup(tom => tom.Current).Returns(new RootEntityWithInheritanceMapping());
+				trackedObjectMock.Setup(tom => tom.IsInteresting).Returns(false);
+				trackedObjectMock.Setup(tom => tom.IsDeleted).Returns(false);
+				trackedObjectMock.Setup(tom => tom.IsModified).Returns(false);
+				trackedObjectMock.Setup(tom => tom.IsDead).Returns(false);
+				trackedObjectMock.Setup(tom => tom.IsWeaklyTracked).Returns(false);
+
+				trackedList.Add(trackedObjectMock.Object, VisitState.After);
+			}
+
+			new TrackedObjectLogFormatter().LogTrackedObjectVisitState(trackedList, "Dictionary", cycleException);
+
+			for (int i = 0; i < 10; i++)
+			{
+				var data = cycleException.Data[$"{i} element in Dictionary"].ToString();
+
+				Assert.AreEqual(expected, data);
+			}
+		}
+	}
+}
